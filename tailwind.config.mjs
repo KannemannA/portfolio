2ab163608d4bb4afd3b8fe8b03c8fa3d4c4a5109
Alejo.K <alejokannemann@gmail.com>
2025-01-16 20:26:53 -0300

    header 80%

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				shine: {
+					"0%": { "background-position": "100%" },
+					"100%": { "background-position": "-100%" },
+				}
+			},
+			animation: {
+				shine: "shine 5s linear infinite",
+			},
+			fontFamily: {
+				mono: ['Major Mono Display', ...defaultTheme.fontFamily.mono],
+			},
+			colors: {
+				verdeFuerte: "#009241",
+				fondoBlanco: "#EEEFF0"
+			}
+		}
 	},
 	plugins: [],
 }
