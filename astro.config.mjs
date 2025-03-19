f9f646ae74fe226d5e730451ee435e261607cd71
Alejo.K <alejokannemann@gmail.com>
2025-03-19 17:26:18 -0300

    Actualización de configuración y eliminación de componentes obsoletos
    - Se agregó la opción `trailingSlash: "always"` en `astro.config.mjs` para asegurar la consistencia en las rutas.
    - Se corrigió la ruta en `tsconfig.json` para que apunte correctamente a `./src/*`.
    - Se eliminaron los componentes `SplashCursor.jsx`, `work.js`, `Proyect.tsx`, y `TestWorker.jsx` que ya no son necesarios.
    - Se actualizó la sección `Header.astro` para eliminar la importación del componente `TestWorker`.
    - Se renombró el componente `Proyect` a `ProjectSection` en `Main.astro` para mayor claridad.
    - Se añadió un nuevo estilo de scrollbar en `globalStyle.css`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,5 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [tailwind(), react()],
   base: '/portfolio/',
+  trailingSlash: "always"
 });
--- src/animations/work.js
+++
@@ -1,5 +0,0 @@
-const test = () => {console.log("hola");
-self.postMessage("hola desde el worker");}
-test();
-
-export default test;
