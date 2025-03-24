97311fe25ed0807ec873f6e81b3b98625fa9f5c6
Alejo.K <alejokannemann@gmail.com>
2025-03-24 09:10:08 -0300

    Actualización de animaciones y estilos en componentes
    - Se eliminaron animaciones de fondo del archivo `tailwind.config.mjs`.
    - Se añadió una nueva animación `shineAnimation` en el componente `ButtonProject.astro`.
    - Se ajustó el estilo del componente `Project.astro` para permitir el desbordamiento automático.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,27 +5,9 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
-			keyframes: {
-				shine: {
-					"0%": { "background-position": "100%" },
-					"100%": { "background-position": "-100%" },
-				},
-				"background-shine": {
-					"from": { "backgroundPosition": "0 0" },
-    				"to": { "backgroundPosition": "-200% 0" }
-				}
-			},
-			animation: {
-				shine: "shine 5s linear infinite",
-				"background-shine": "background-shine 2s linear infinite"
-			},
 			fontFamily: {
 				mono: ['Major Mono Display', ...defaultTheme.fontFamily.mono],
 				display: ['Bungee Shade', ...defaultTheme.fontFamily.serif],
-			},
-			colors: {
-				verdeFuerte: "#009241",
-				fondoBlanco: "#EEEFF0"
 			}
 		}
 	},
